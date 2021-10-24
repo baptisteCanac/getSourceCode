8ac660142c3b426692f04e5eedeba71b70a7ab22
Baptiste <74823118+baptisteCanac@users.noreply.github.com>
2021-10-24 20:43:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import pyperclip
+
+website = input("website complete adress (ex:https://google.com) : ")
+
+driver = webdriver.Edge(executable_path="msedgedriver.exe")
+driver.get(website)
+
+htmlSource = driver.page_source
+print(htmlSource)
+pyperclip.copy(htmlSource)
+
+driver.close()
+
+print("The text as been copied, you can copy it in a code or text editor")
